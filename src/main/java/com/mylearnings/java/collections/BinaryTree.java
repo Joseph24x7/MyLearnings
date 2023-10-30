f5fa898a57b69916607825aeb0b7c713454ee48f
joseph24x7 <joseph2471994@gmail.com>
2023-10-30 19:41:03 +0530

    check whether binary tree is identical

--- src/main/java/com/mylearnings/java/collections/BinaryTree.java
+++ src/main/java/com/mylearnings/java/collections/BinaryTree.java
@@ -13,6 +13,16 @@ class BSTMainClass {
         bst.add(1);
         bst.add(4);
 
+        BinaryTree bst2 = new BinaryTree();
+        bst2.add(5);
+        bst2.add(3);
+        bst2.add(8);
+        bst2.add(1);
+        bst2.add(4);
+
+        Boolean areBinaryTreeIdentical = areBinaryTreeIdentical(bst.getHead(), bst2.getHead());
+        System.out.println("areBinaryTreeIdentical: " +areBinaryTreeIdentical);
+
         System.out.print("In-order traversal: ");
         bst.printInOrder();
         System.out.println();
@@ -21,6 +31,32 @@ class BSTMainClass {
         System.out.println();
         System.out.print("Post-order traversal: ");
         bst.printPostOrder();
+
+    }
+
+    private static Boolean areBinaryTreeIdentical(TreeNode bst1, TreeNode bst2) {
+
+        // Check if both trees are null, indicating they are identical.
+        if (bst1 == null && bst2 == null) {
+            return true;
+        }
+
+        // If one tree is null and the other is not, they are not identical.
+        if (bst1 == null || bst2 == null) {
+            return false;
+        }
+
+        // Check if the current nodes have the same value.
+        if (!bst1.getVal().equals(bst2.getVal())) {
+            return false;
+        }
+
+        // Recursively check the left and right subtrees.
+        boolean leftIdentical = areBinaryTreeIdentical(bst1.getLeft(), bst2.getLeft());
+        boolean rightIdentical = areBinaryTreeIdentical(bst1.getRight(), bst2.getRight());
+
+        // Both subtrees must be identical for the trees to be identical.
+        return leftIdentical && rightIdentical;
 
     }
 
