402e97dbb639e3185087e58789916afd71c8d46a
Joseph24x7 <Joseph2471994@gmail.com>
2026-01-02 21:18:20 +0530

    Leet code 150 - Jump game greedy

--- src/main/java/com/mylearnings/java/datastructures/leetcode150/greedy/JumpGame1.java
+++ src/main/java/com/mylearnings/java/datastructures/leetcode150/greedy/JumpGame1.java
@@ -1,32 +1,25 @@
 package com.mylearnings.java.datastructures.leetcode150.greedy;
 
-public class JumGame1 {
+public class JumpGame1 {
 
-    public static void main(String[] args) {
-        JumGame1 jumGame1 = new JumGame1();
-        jumGame1.canJump(new int[]{1, 2, 1, 1, 1});
+    void main() {
+        JumpGame1 jumpGame1 = new JumpGame1();
+        System.out.println(jumpGame1.canJump(new int[]{2, 3, 1, 1, 4}));
     }
 
     public boolean canJump(int[] nums) {
-
         int index = 0;
         int maxJump = 0;
         while (index < nums.length) {
-
             if (index > maxJump) {
                 return false;
             }
-
             maxJump = Math.max(maxJump, index + nums[index]);
-
-            if(maxJump >= nums.length) {
+            if (maxJump >= nums.length) {
                 return true;
             }
-
             index++;
-
         }
-
         return true;
 
     }
--- src/main/java/com/mylearnings/java/datastructures/leetcode150/greedy/JumpGameII.java
+++ src/main/java/com/mylearnings/java/datastructures/leetcode150/greedy/JumpGameII.java
@@ -1,4 +1,44 @@
 package com.mylearnings.java.datastructures.leetcode150.greedy;
 
 public class JumpGameII {
+
+    public static void main(String[] args) {
+        JumpGameII jumpGameII = new JumpGameII();
+        System.out.println(jumpGameII.jump(new int[]{2, 1})); // 1
+        System.out.println(jumpGameII.jump(new int[]{2, 3, 1, 1, 4})); // 2
+    }
+
+    public int jump(int[] nums) {
+
+        if (nums.length <= 1) {
+            return 0;
+        }
+
+        int index = 0;
+        int jumps = 0;
+
+        while (index < nums.length - 1) {
+            int maxReach = 0;
+            int nextIndex = index;
+
+            for (int step = 1; step <= nums[index]; step++) {
+                int pos = index + step;
+                if (pos >= nums.length - 1) {
+                    return jumps + 1;
+                }
+
+                int reach = pos + nums[pos];
+                if (reach > maxReach) {
+                    maxReach = reach;
+                    nextIndex = pos;
+                }
+            }
+
+            index = nextIndex;
+            jumps++;
+        }
+
+        return jumps;
+    }
 }
+
